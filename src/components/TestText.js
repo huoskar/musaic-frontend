51d4d4ee648b030fe932bd1bf2e201c1866a4b2f
Andreas Mann <andreasmann94@gmail.com>
2018-10-13 10:37:08 +0200

    First commit

---
+++ src/components/TestText.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class TestText extends Component {
+  render() {
+    return (
+      <div>
+        <p>THIS IS A TEST TEXT</p>
+      </div>
+    )
+  }
+}
+
+export default TestText;
---
+++ src/pages/UploadPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class UploadPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>TODO: UPLOAD PAGE</h1>
+      </div>
+    )
+  }
+}
+
+export default UploadPage;
